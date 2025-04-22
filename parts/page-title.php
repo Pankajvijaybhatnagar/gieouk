1f0a220fc27e80ea8366b6c366de25f8756f7ea0
ankitjnvynr <ankitbkana@outlook.com>
2025-04-22 18:09:29 +0530

    added day  1 pic

--- parts/page-title.php
+++ parts/page-title.php
@@ -8,7 +8,7 @@
                     <h2><?php if(isset($page_title)&&!empty($page_title)) { echo $page_title; } ?></h2>
                     <div class="thm-breadcrumb__box">
                         <ul class="thm-breadcrumb list-unstyled">
-                            <li><a href="index.php">Home</a></li>
+                            <li><a href="./">Home</a></li>
                             <li><span class="icon-arrow-left"></span></li>
                             <li><?php if(isset($page_title)&&!empty($page_title)) { echo $page_title; } ?></li>
                         </ul>
