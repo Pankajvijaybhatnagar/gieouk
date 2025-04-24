be858abf3e758fb1071186cbac694228af7e3e7f
ankitjnvynr <ankitbkana@outlook.com>
2025-04-24 15:22:19 +0530

    update favicon

--- parts/page-title.php
+++ parts/page-title.php
@@ -7,11 +7,11 @@
                 <div class="page-header__inner">
                     <h2><?php if(isset($page_title)&&!empty($page_title)) { echo $page_title; } ?></h2>
                     <div class="thm-breadcrumb__box">
-                        <ul class="thm-breadcrumb list-unstyled">
+                        <!-- <ul class="thm-breadcrumb list-unstyled">
                             <li><a href="./">Home</a></li>
                             <li><span class="icon-arrow-left"></span></li>
                             <li><?php //if(isset($page_title)&&!empty($page_title)) { echo $page_title; } ?></li>
-                        </ul>
+                        </ul> -->
                     </div>
                 </div>
             </div>
