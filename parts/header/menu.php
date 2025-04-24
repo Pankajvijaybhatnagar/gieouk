d05e10fb1bc631f300aac123e83aa80bd909c4d9
ankitjnvynr <ankitbkana@outlook.com>
2025-04-24 12:21:37 +0530

    update in join gita

--- parts/header/menu.php
+++ parts/header/menu.php
@@ -3,56 +3,49 @@
 		<a href="https://gieogita.org.uk/">Home</a>
 	</li>
 	<li>
-		<a href="https://gieogita.org.uk/">About Us</a>
+		<a href="https://gieogita.org.uk/">About</a>
 	</li>
 	<li>
 		<a href="https://parivaar.gieogita.org/">Join GIEO Gita</a>
 	</li>
 
 	<li class="dropdown">
-		<a href="#">Pages</a>
-		<ul>
-			<li><a href="https://gieogita.org.uk/team.php">Team</a></li>
-			<li><a href="https://gieogita.org.uk/testimonials.php">Testimonials</a></li>
-			<li><a href="https://gieogita.org.uk/faq.php">Faq</a></li>
-			<li><a href="https://gieogita.org.uk/404.php">404 Error</a></li>
-			<li><a href="https://gieogita.org.uk/coming-soon.php">Coming Soon</a></li>
-		</ul>
-	</li>
-
-	<li class="dropdown">
 		<a href="#">Seva</a>
 		<ul>
-			<li><a href="https://gieogita.org.uk/seva-projects.php">Projects</a></li>
-			<li><a href="https://gieogita.org.uk/seva-details.php">Project Details</a></li>
+			<li><a href="https://gieogita.org.uk/gau-seva">Gau Seva</a></li>
 		</ul>
 	</li>
 
 	<li class="dropdown">
 		<a href="#">Education</a>
 		<ul>
-			<li><a href="https://gieogita.org.uk/education-projects.php">Projects</a></li>
-			<li><a href="https://gieogita.org.uk/education-details.php">Project Details</a></li>
+			<li><a href="https://gieogita.org.uk/bhagwat-gita-course">An Introduction to Bhagwat Gita</a></li>
 		</ul>
 	</li>
+
+	
 
 	<li class="dropdown">
 		<a href="#">Events</a>
 		<ul>
-			<li><a href="https://gieogita.org.uk/events.php">Projects</a></li>
-			<li><a href="https://gieogita.org.uk/event-details.php">Project Details</a></li>
+			<li><a href="https://gieogita.org.uk/diwali-cup">Diwali Cup</a></li>
+			<li><a href="https://gieogita.org.uk/manchester-gita-festival-2024">Manchester Gita Festival 2024</a></li>
+			<li><a href="https://gieogita.org.uk/manchester-gita-festival-2025">Manchester Gita Festival 2024</a></li>
 		</ul>
 	</li>
 
-	<li class="dropdown">
+	<!-- <li class="dropdown">
 		<a href="#">Blogs</a>
 		<ul>
 			<li><a href="https://gieogita.org.uk/blogs.php">Blog</a></li>
 			<li><a href="https://gieogita.org.uk/blog-details.php">Blog Details</a></li>
 		</ul>
-	</li>
+	</li> -->
 
 	<li>
-		<a href="https://gieogita.org.uk/contact.php">Contact</a>
+		<a href="https://gieogita.org.uk/gallery">Gallery</a>
+	</li>
+	<li>
+		<a href="https://gieogita.org.uk/">Contact</a>
 	</li>
 </ul>
