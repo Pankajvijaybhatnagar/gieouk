9b1ab78b79019a678f4d8f13fb52cb6b8d13fd5f
ankitjnvynr <ankitbkana@outlook.com>
2025-04-24 12:54:46 +0530

    added the events three on homepage

--- parts/home3/project.php
+++ parts/home3/project.php
@@ -5,46 +5,53 @@
       <div class="project-three__top">
         <div class="section-title text-left">
           <div class="section-title__tagline-box">
-            <span class="section-title__tagline">Our Gallery</span>
+            <span class="section-title__tagline">Events</span>
           </div>
-          <h2 class="section-title__title">Moments from Manchester's<br> Gita Festival Celebrations</h2>
+          <h2 class="section-title__title">Moments from Manchester's Gita Festival Celebrations</h2>
         </div>
-        <div class="project-three__btn-box">
+        <!-- <div class="project-three__btn-box">
           <a href="index.php" class="project-three__btn thm-btn">
             View More<span class="icon-arrow-left"></span>
           </a>
-        </div>
+        </div> -->
       </div>
       <div class="row">
         <!--Project Three Single Start-->
         <div class="col-xl-4 col-lg-4 wow fadeInLeft" data-wow-delay="100ms">
+          <a href="day-1.php">
           <div class="project-three__single">
             <div class="project-three__img">
               <img style="aspect-ratio: 410/299;" src="assets/images/backgrounds/11th.jpeg" alt="Gita Seminar">
             </div>
+            <h3> Day 1: Opening Night & Yoga Through Culture & Science
+            </h3>
           </div>
+          </a>
         </div>
-        <!--Project Three Single End-->
-
-        <!--Project Three Single Start-->
-        <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="200ms">
+        <div class="col-xl-4 col-lg-4 wow fadeInLeft" data-wow-delay="100ms">
+          <a href="day-1.php">
           <div class="project-three__single">
             <div class="project-three__img">
               <img src="assets/images/backgrounds/img2.png" alt="Yoga in the Park">
             </div>
+            <h3>  Day 2: Shobha Yatra & Bhakti Yoga Evening
+            </h3>
           </div>
+          </a>
+        </div>
+        <div class="col-xl-4 col-lg-4 wow fadeInLeft" data-wow-delay="100ms">
+          <a href="day-1.php">
+          <div class="project-three__single">
+            <div class="project-three__img">
+            <img style="aspect-ratio: 410/299;" src="assets/images/backgrounds/img3.jpg" alt="Bhakti Evening">
+            </div>
+            <h3> Day 1: Opening Night & Yoga Through Culture & Science
+            </h3>
+          </div>
+          </a>
         </div>
         <!--Project Three Single End-->
 
-        <!--Project Three Single Start-->
-        <div class="col-xl-4 col-lg-4 wow fadeInRight" data-wow-delay="300ms">
-          <div class="project-three__single">
-            <div class="project-three__img">
-              <img style="aspect-ratio: 410/299;" src="assets/images/backgrounds/img3.jpg" alt="Bhakti Evening">
-            </div>
-          </div>
-        </div>
-        <!--Project Three Single End-->
       </div>
     </div>
   </div>
