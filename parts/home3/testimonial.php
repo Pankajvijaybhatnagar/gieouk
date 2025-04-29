0bae29242cc0418379548693202f8d53b7eaa9e9
ankitjnvynr <ankitbkana@outlook.com>
2025-04-29 13:24:49 +0530

    testimonail added

--- parts/home3/testimonial.php
+++ parts/home3/testimonial.php
@@ -1,3 +1,100 @@
+
+<?php
+$testimonials = [
+    [
+        'text' => 'Many thanks for hosting me yesterday and also for making the transport arrangements. I trust that the conference and festival is very successful. I was pleased to join you. Well done in organising such an important event.',
+        'name' => 'Bob Blackman MP',
+        'designation' => 'Padma Shri, Chairman APPG for British Hindus, Chairman 1922 Committee',
+    ],
+    [
+        'text' => 'On behalf of VOICE, Manchester and Sewaday NorthWest I bow to Gyanandji Maharaj ji for inspiring the Gita festival to take place in Manchester. Hugely grateful to Kapoor parivar for bringing together so many varied Hindu organisations together. Dhanyavaad to all the visible and non-visible karyakartas who helped to execute the event and spread the message of Gita ji.',
+        'name' => 'Dr Hardik Bansali',
+        'designation' => 'Chairman of VOICE and SewaDay',
+    ],
+    [
+        'text' => 'On behalf of Northwest Odiya Samaj and myself and my wife Rekha I would like to thank all volunteers and organisations who have worked together wonderfully to showcase a unique, vibrant and inclusive environment with the key message of Love Peace and Harmony. Special thanks to the trio Kapoor’s and their family for the incredible energy, dedication and drive.',
+        'name' => 'Prof Siba Senapati',
+        'designation' => 'Northwest Odisha Samaj',
+    ],
+    [
+        'text' => 'On behalf of JETUK Manchester we extend our heartfelt gratitude for giving us an opportunity to be part of this great initiative and little service we were given. It’s a feast to my eyes to see all dharmic organisations on one stage. It’s so inspiring to see the good work of everyone.',
+        'name' => 'Dinesh Ramanuja Das',
+        'designation' => 'JET UK',
+    ],
+    [
+        'text' => 'I bow down with utmost humility to everyone involved in achieving this incredible success of the Gita Festival Manchester! It’s our hope and prayer that the Gita Festival becomes a regular annual event in Manchester.',
+        'name' => 'Dr Nachiappan',
+        'designation' => 'Manchester Shiva Temple',
+    ],
+    [
+        'text' => 'Absolutely stupendous work over all the months culminating in a fantastic display of your message - Peace, Love & Harmony over the 3 days. Fabulous display of our culture from very talented children & adults in the community.',
+        'name' => 'Nrithya Ramoham',
+        'designation' => 'Nrithyakala',
+    ],
+    [
+        'text' => 'I had the wonderful opportunity to witness Nishkaama Karm in a number of people including Kapoor Brothers and many volunteers. This goes to say that all the people involved not only read Gita but follow it in their lives!',
+        'name' => 'Dr Anand Kulkarni',
+        'designation' => 'Hindu Knowledge Academy',
+    ],
+    [
+        'text' => 'This has truly proved Love, Peace, Harmony and especially "Vividhata mein Ekta" mantra. It was quite an overwhelming and emotional experience, especially yesterday in the parade.',
+        'name' => 'Poonam Patil',
+        'designation' => 'Manchester Marathi Mandal',
+    ],
+    [
+        'text' => 'The mammoth Manchester International Gita Festival bringing 42 organisations together, spreading the message of Peace, Love and Harmony through the teachings of the Srimad Bhagawad Gita and Shri Krishna\'s divine leelas has been a grand success.',
+        'name' => 'Dr Chitra Pandilwar',
+        'designation' => 'Radha Madhav Society',
+    ],
+    [
+        'text' => 'I truly enjoyed and feel blessed today. Wonderful arrangements and seeing a lot of Dharmic organisations on one forum was a feast to my eyes.',
+        'name' => 'Anonymous',
+        'designation' => 'JET UK Attendee',
+    ],
+    [
+        'text' => 'Big salutes to Kapoor brothers who took initiation to this great event. Organising such a great event is not an easy task. Thank you so much for this opportunity to perform in this great festival.',
+        'name' => 'PYDA International',
+        'designation' => '',
+    ],
+    [
+        'text' => 'भक्ति (कार्य) और प्रसाद (कान्हा जी) दोनों का आनंद जो मन आज उठा रहा है उसमें जो ऊर्जा आप भाइयों ने भाव रूप में भरी है उसके लिए ह्रदय से आभार। साथ ही आभार उन्हें भी जो दिन रात इस महान कार्य में जुटे रहे। नमन सभी की मेहनत को।',
+        'name' => 'Anonymous',
+        'designation' => '',
+    ],
+    [
+        'text' => 'I wanted to extend my heartfelt congratulations to you all for the tremendous success of the 3-day Gita Festival in Manchester. It was truly inspiring to see how you brought the Hindu community together in such a meaningful and uplifting way.',
+        'name' => 'Mayur',
+        'designation' => 'BAPS',
+    ],
+    [
+        'text' => 'Jai Shree Krishna Aneetji and all Kapoor Parivar for giving us this excellent opportunity to be part of this unique Gita festival of our Manchester. Thanks from Akash too for the opportunity to be part of the seva-team on stage.',
+        'name' => 'Kaushik & Akash',
+        'designation' => 'Bolton Hindu Forum',
+    ],
+    [
+        'text' => 'Please let me congratulate you and team for a well organised GIEO GITA Mahotsav over the past three days under the auspices of his holiness, Swami Gyananand ji Maharaj; spreading the message of Peace, Love and Harmony.',
+        'name' => 'Dr Kaushik Chakraborty',
+        'designation' => 'Gen Secretary, Vishwa Hindu Parishad Manchester',
+    ],
+    [
+        'text' => 'I travelled from Birmingham to do Sewa at GIEO Gita Festival. It was an amazing experience, enjoyed the quiz and cultural program. Gyanandji Maharaji\'s talk was very touching and inspirational.',
+        'name' => 'Bhavna Dhorajia',
+        'designation' => '',
+    ],
+    [
+        'text' => 'Thank you so much for giving us an opportunity to be a volunteer and giving us chance to offer our Seva at GIEO Gita festival.',
+        'name' => 'Mina Mistry',
+        'designation' => '',
+    ],
+];
+
+?>
+
+
+
+
+
+
 <!--Testimonial Three Start-->
 <section class="testimonial-three">
     <div class="container">
@@ -47,51 +144,30 @@
                             "1320": { "spaceBetween": 30, "slidesPerView": 1 }
                         }
                     }'>
-                        <div class="swiper-wrapper">
-                            <!--Review One Start-->
+                    <div class="swiper-wrapper">
+                        <?php foreach ($testimonials as $testimonial): ?>
                             <div class="swiper-slide">
                                 <div class="testimonial-three__single">
                                     <div class="testimonial-three__text-box">
                                         <div class="testimonial-three__quote">
                                             <span class="icon-quote"></span>
                                         </div>
-                                        <p class="testimonial-three__text">Attending the Gieogita session with Gitaminishi Gyannand Ji Maharaj was truly life-changing. His divine guidance and simplified explanation of Bhagvad Gita brought me inner peace and clarity. A must-attend spiritual experience in Manchester!</p>
+                                        <p class="testimonial-three__text"><?= htmlspecialchars($testimonial['text']) ?></p>
                                     </div>
                                     <div class="testimonial-three__client-info">
                                         <div class="testimonial-three__client-img">
-                                            <img src="assets/images/testimonial/testimonial-three-client-img-1.jpg" alt="">
+                                            <img src="<?= htmlspecialchars($testimonial['image']) ?>" alt="">
                                         </div>
                                         <div class="testimonial-three__client-content">
-                                            <h3><a href="testimonials.php">Meera Patel</a></h3>
-                                            <p>Spiritual Seeker</p>
+                                            <h3><a href="testimonials.php"><?= htmlspecialchars($testimonial['name']) ?></a></h3>
+                                            <p><?= htmlspecialchars($testimonial['designation']) ?></p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <!--Review One End-->
+                        <?php endforeach; ?>
+</div>
 
-                            <!--Review Two Start-->
-                            <div class="swiper-slide">
-                                <div class="testimonial-three__single">
-                                    <div class="testimonial-three__text-box">
-                                        <div class="testimonial-three__quote">
-                                            <span class="icon-quote"></span>
-                                        </div>
-                                        <p class="testimonial-three__text">Gyannand Ji Maharaj’s discourse in Manchester was enlightening. His deep knowledge and ability to connect ancient wisdom to today’s life made the session unforgettable. I left feeling uplifted and inspired.</p>
-                                    </div>
-                                    <div class="testimonial-three__client-info">
-                                        <div class="testimonial-three__client-img">
-                                            <img src="assets/images/testimonial/testimonial-three-client-img-2.jpg" alt="">
-                                        </div>
-                                        <div class="testimonial-three__client-content">
-                                            <h3><a href="testimonials.php">Ramesh Verma</a></h3>
-                                            <p>Devotee</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!--Review Two End-->
-                        </div>
                     </div>
                 </div>
             </div>
