920885acd8b167c817e9f18a0ceafd4dd84f76af
ankitjnvynr <ankitbkana@outlook.com>
2025-04-29 13:15:41 +0530

    dv

--- parts/mobile-nav.php
+++ parts/mobile-nav.php
@@ -15,18 +15,15 @@
             <ul class="mobile-nav__contact list-unstyled">
                 <li>
                     <i class="fa fa-envelope"></i>
-                    <a href="mailto:needhelp@packageName__.com">needhelp@envens.com</a>
+                    <a href="mailto:info@gieogita.org.uk">info@gieogita.org.uk</a>
                 </li>
-                <li>
-                    <i class="fa fa-phone-alt"></i>
-                    <a href="tel:666-888-0000">666 888 0000</a>
-                </li>
+                
             </ul><!-- /.mobile-nav__contact -->
             <div class="mobile-nav__top">
                 <div class="mobile-nav__social">
-                    <a href="#" class="fab fa-twitter"></a>
+                    
                     <a href="#" class="fab fa-facebook-square"></a>
-                    <a href="#" class="fab fa-pinterest-p"></a>
+                    
                     <a href="#" class="fab fa-instagram"></a>
                 </div><!-- /.mobile-nav__social -->
             </div><!-- /.mobile-nav__top -->
